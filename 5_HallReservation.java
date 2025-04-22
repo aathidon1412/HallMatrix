8a7c7a55e3a7bd971159ef8c6b277f4971ce92b8
Aathikesavaa <aathikesavaasundararajan@gmail.com>
2025-04-22 20:56:14 +0530

    Update 5_HallReservation.java

--- 5_HallReservation.java
+++ 5_HallReservation.java
@@ -1,3 +1,5 @@
+import java.util.*;
+
 class HallReservation 
 {   static int idCounter=1;
     int reservationId;
@@ -17,4 +19,39 @@ class HallReservation
     public String toString()
     {   return "ID:"+reservationId+"| Dept:"+department+"| Floor:"+floor+
         "| Room:"+roomNo+"| User:"+user.getName()+"| Purpose:"+purpose+"| Status:"+status;}
+}
+
+class ReservationManager 
+{   ArrayList<HallReservation> reservations=new ArrayList<>();
+
+    void bookHall(String dept,String floor,String room,String purpose,User user)
+    {   for(HallReservation r:reservations)
+        {   if(r.department.equalsIgnoreCase(dept)&&r.floor.equalsIgnoreCase(floor)&&r.roomNo.equalsIgnoreCase(room))
+            {   System.out.println("Room already Booked!!");
+                return;
+            }
+        }
+
+        HallReservation res=new HallReservation(dept,floor,room,purpose,user);
+        reservations.add(res);
+        System.out.println("Reservation ID:"+res.reservationId+" Created(Pending)!!");
+    }
+    void viewAll()
+    {   if(reservations.isEmpty())
+        {   System.out.println("No reservation Yet!!");
+            return;
+        }
+        for(HallReservation r:reservations)
+        {   System.out.println(r);}
+    }
+    void approveOrReject(int id,Boolean approve)
+    {   for(HallReservation r:reservations)
+        {   if(r.reservationId==id)
+            {   r.status=approve?"Approved":"Rejected";
+                System.out.println("Reservation"+(approve?"Approved":"Rejected"));
+                return;
+            }
+        }
+        System.out.println("Reservation Not Found!!");
+    }
 }
